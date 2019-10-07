7ad8f84c662ffab148312b942d54e0682bae7db4
Manender Kumar <manender.kumar@mamsys.com>
2019-10-07 18:15:26 +0530

    table updates

--- src/app/tables/table-container/table-container.component.ts
+++ src/app/tables/table-container/table-container.component.ts
@@ -9,11 +9,46 @@ import { TableData } from '../table-data';
 })
 export class TableContainerComponent implements OnInit {
 
+  columnSchema = [];
   tableData = [];
+  tableConfig: any;
   constructor() { }
 
   ngOnInit() {
     this.tableData = TableData;
+    this.columnSchema = [
+      {
+        title: 'Name',
+        name: 'name',
+        filtering: { filterString: '', placeholder: 'Filter by name' }
+      },
+      {
+        title: 'Position',
+        name: 'position',
+        sort: false,
+        filtering: { filterString: '', placeholder: 'Filter by position' }
+      },
+      {
+        title: 'Office',
+        className: ['office-header', 'text-success'],
+        name: 'office',
+        sort: 'asc'
+      },
+      {
+        title: 'Extn.',
+        name: 'ext',
+        sort: '',
+        filtering: { filterString: '', placeholder: 'Filter by extn.' }
+      },
+      { title: 'Start date', className: 'text-warning', name: 'startDate' },
+      { title: 'Salary ($)', name: 'salary' }
+    ];
+    this.tableConfig = {
+      paging: true,
+      sorting: { columns: this.columnSchema },
+      filtering: { filterString: '' },
+      className: ['table-striped', 'table-bordered']
+    };
   }
 
 }
--- src/app/tables/table/table.component.ts
+++ src/app/tables/table/table.component.ts
@@ -11,48 +11,17 @@ export class TableComponent implements OnInit {
 
   length = 0;
 
-  @Input() columns: any[] = [
-    {
-      title: 'Name',
-      name: 'name',
-      filtering: { filterString: '', placeholder: 'Filter by name' }
-    },
-    {
-      title: 'Position',
-      name: 'position',
-      sort: false,
-      filtering: { filterString: '', placeholder: 'Filter by position' }
-    },
-    {
-      title: 'Office',
-      className: ['office-header', 'text-success'],
-      name: 'office',
-      sort: 'asc'
-    },
-    {
-      title: 'Extn.',
-      name: 'ext',
-      sort: '',
-      filtering: { filterString: '', placeholder: 'Filter by extn.' }
-    },
-    { title: 'Start date', className: 'text-warning', name: 'startDate' },
-    { title: 'Salary ($)', name: 'salary' }
-  ];
   @Input() page = 1;
   @Input() itemsPerPage = 10;
   @Input() maxSize = 5;
   @Input() numPages = 1;
 
   @Input() data: any[];
-  @Input() config = {
-    paging: true,
-    sorting: { columns: this.columns },
-    filtering: { filterString: '' },
-    className: ['table-striped', 'table-bordered']
-  };
+  @Input() columns: any[];
 
-  constructor() {
-  }
+  @Input() config: any;
+
+  constructor() {}
 
   ngOnInit(): void {
     if (!this.data) {
